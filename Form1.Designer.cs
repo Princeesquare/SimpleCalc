e63086e06a3041a5ecc245ca9a18a9572f0edce7
WIN ENG <WIN ENG@GE121-PC10>
2023-03-06 03:25:03 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace SimpleCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Sin = new System.Windows.Forms.Button();
+            this.Cos = new System.Windows.Forms.Button();
+            this.Tan = new System.Windows.Forms.Button();
+            this.Exp = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Value 1: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(142, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(95, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(142, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(95, 113);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(142, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Enter Value 2: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Enter Value 3: ";
+            // 
+            // Sin
+            // 
+            this.Sin.Location = new System.Drawing.Point(15, 173);
+            this.Sin.Name = "Sin";
+            this.Sin.Size = new System.Drawing.Size(101, 23);
+            this.Sin.TabIndex = 3;
+            this.Sin.Text = "Calculate Sin";
+            this.Sin.UseVisualStyleBackColor = true;
+            this.Sin.Click += new System.EventHandler(this.Sin_Click);
+            // 
+            // Cos
+            // 
+            this.Cos.Location = new System.Drawing.Point(136, 173);
+            this.Cos.Name = "Cos";
+            this.Cos.Size = new System.Drawing.Size(101, 23);
+            this.Cos.TabIndex = 3;
+            this.Cos.Text = "Calculate Cos";
+            this.Cos.UseVisualStyleBackColor = true;
+            this.Cos.Click += new System.EventHandler(this.Cos_Click);
+            // 
+            // Tan
+            // 
+            this.Tan.Location = new System.Drawing.Point(15, 227);
+            this.Tan.Name = "Tan";
+            this.Tan.Size = new System.Drawing.Size(101, 23);
+            this.Tan.TabIndex = 3;
+            this.Tan.Text = "Calculate Tan";
+            this.Tan.UseVisualStyleBackColor = true;
+            this.Tan.Click += new System.EventHandler(this.Tan_Click);
+            // 
+            // Exp
+            // 
+            this.Exp.Location = new System.Drawing.Point(136, 227);
+            this.Exp.Name = "Exp";
+            this.Exp.Size = new System.Drawing.Size(101, 23);
+            this.Exp.TabIndex = 3;
+            this.Exp.Text = "Calculate Exp";
+            this.Exp.UseVisualStyleBackColor = true;
+            this.Exp.Click += new System.EventHandler(this.Exp_Click);
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.Location = new System.Drawing.Point(12, 295);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(80, 24);
+            this.result.TabIndex = 4;
+            this.result.Text = "Result: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 361);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.Exp);
+            this.Controls.Add(this.Tan);
+            this.Controls.Add(this.Cos);
+            this.Controls.Add(this.Sin);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Sin;
+        private System.Windows.Forms.Button Cos;
+        private System.Windows.Forms.Button Tan;
+        private System.Windows.Forms.Button Exp;
+        private System.Windows.Forms.Label result;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Sin_Click(object sender, EventArgs e)
+        {
+            double x,y,z, res;
+            if (textBox1.Text != null && textBox2.Text == "" && textBox3.Text == "")
+            {
+                x = Convert.ToDouble(textBox1.Text);
+                res = Math.Sin(x);
+                MessageBox.Show(res.ToString());
+                result.Text = result.Text + res.ToString();
+            }
+           else if (textBox1.Text != null && textBox2.Text != null)
+            {
+                x = Convert.ToDouble(textBox1.Text);
+                y = Convert.ToDouble(textBox2.Text);
+                res = Math.Sin(x) * Math.Sin(y);
+                MessageBox.Show(res.ToString());
+                result.Text = result.Text + res.ToString();
+            }
+            else if (textBox1.Text != null && textBox2.Text != null && textBox3.Text != null)
+            {
+                x = Convert.ToDouble(textBox1.Text);
+                y = Convert.ToDouble(textBox2.Text);
+                z = Convert.ToDouble(textBox3.Text);
+                res = Math.Sin(x) * Math.Sin(y) * Math.Sin(z);
+                MessageBox.Show(res.ToString());
+                result.Text = result.Text + res.ToString();
+            }
+        }
+
+        private void Cos_Click(object sender, EventArgs e)
+        {
+            double x, y, z, res;
+            if (textBox1.Text != null && textBox2.Text == "" && textBox3.Text == "")
+            {
+                x = Convert.ToDouble(textBox1.Text);
+                res = Math.Cos(x);
+                MessageBox.Show(res.ToString());
+                result.Text = result.Text + res.ToString();
+            }
+            else if (textBox1.Text != null && textBox2.Text != null)
+            {
+                x = Convert.ToDouble(textBox1.Text);
+                y = Convert.ToDouble(textBox2.Text);
+                res = Math.Cos(x) * Math.Cos(y);
+                MessageBox.Show(res.ToString());
+                result.Text = result.Text + res.ToString();
+            }
+            else if (textBox1.Text != null && textBox2.Text != null && textBox3.Text != null)
+            {
+                x = Convert.ToDouble(textBox1.Text);
+                y = Convert.ToDouble(textBox2.Text);
+                z = Convert.ToDouble(textBox3.Text);
+                res = Math.Cos(x) * Math.Cos(y) * Math.Cos(z);
+                MessageBox.Show(res.ToString());
+                result.Text = result.Text + res.ToString();
+            }
+
+        }
+
+        private void Tan_Click(object sender, EventArgs e)
+        {
+            double x, y, z, res;
+            if (textBox1.Text != null && textBox2.Text == "" && textBox3.Text == "")
+            {
+                x = Convert.ToDouble(textBox1.Text);
+                res = Math.Tan(x);
+                MessageBox.Show(res.ToString());
+                result.Text = result.Text + res.ToString();
+            }
+            else if (textBox1.Text != null && textBox2.Text != null)
+            {
+                x = Convert.ToDouble(textBox1.Text);
+                y = Convert.ToDouble(textBox2.Text);
+                res = Math.Tan(x) * Math.Tan(y);
+                MessageBox.Show(res.ToString());
+                result.Text = result.Text + res.ToString();
+            }
+            else if (textBox1.Text != null && textBox2.Text != null && textBox3.Text != null)
+            {
+                x = Convert.ToDouble(textBox1.Text);
+                y = Convert.ToDouble(textBox2.Text);
+                z = Convert.ToDouble(textBox3.Text);
+                res = Math.Tan(x) * Math.Tan(y) * Math.Tan(z);
+                MessageBox.Show(res.ToString());
+                result.Text = result.Text + res.ToString();
+            }
+        }
+
+        private void Exp_Click(object sender, EventArgs e)
+        {
+            double x, y, z, res;
+            if (textBox1.Text != null && textBox2.Text == "" && textBox3.Text == "")
+            {
+                x = Convert.ToDouble(textBox1.Text);
+                res = Math.Exp(x);
+                MessageBox.Show(res.ToString());
+                result.Text = result.Text + res.ToString();
+            }
+            else if (textBox1.Text != null && textBox2.Text != null)
+            {
+                x = Convert.ToDouble(textBox1.Text);
+                y = Convert.ToDouble(textBox2.Text);
+                res = Math.Exp(x) * Math.Exp(y);
+                MessageBox.Show(res.ToString());
+                result.Text = result.Text + res.ToString();
+            }
+            else if (textBox1.Text != null && textBox2.Text != null && textBox3.Text != null)
+            {
+                x = Convert.ToDouble(textBox1.Text);
+                y = Convert.ToDouble(textBox2.Text);
+                z = Convert.ToDouble(textBox3.Text);
+                res = Math.Exp(x) * Math.Exp(y) * Math.Exp(z);
+                MessageBox.Show(res.ToString());
+                result.Text = result.Text + res.ToString();
+            }
+        }
+    }
+}
